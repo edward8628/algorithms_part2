a4a3ff666ad85f89b2442e2b8ffd89ce179543bf
fennnnnn <shenjian8628@gmail.com>
2015-11-01 18:11:06 -0500

    move SAP test client under SAP.java

--- week1/SAP.java
+++ week1/SAP.java
@@ -30,7 +30,15 @@ public class SAP {
         return 0;
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) { 
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt(); 
+            int length =sap.length(v,w); 
+            int ancestor = sap.ancestor(v, w);
+        }
     }
 }
