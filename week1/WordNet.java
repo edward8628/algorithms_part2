d3adc28014dc983469e5981c64df090c74586b4e
fennnnnn <shenjian8628@gmail.com>
2015-11-01 18:39:53 -0500

    fix comments position

--- week1/WordNet.java
+++ week1/WordNet.java
@@ -2,37 +2,40 @@ import java.lang.*;
 import java.util.*;
 
 public class WordNet {
+    
+    
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms)
-    // returns all WordNet nouns
     {
+
     }
 
+    // returns all WordNet nouns
     public Iterable<String> nouns()
-    // is the word a WordNet noun?
     {
         return new Stack<String>();
     }
 
+    // is the word a WordNet noun?
     public boolean isNoun(String word)
-    // distance between nounA and nounB (defined below)
     {
         return false;
     }
 
+    // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB)
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
     {
         return 0;
     }
 
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB)
-    // do unit testing of this class
     {
         return null;
     }
 
+    // do unit testing of this class
     public static void main(String[] args) {
     }
 }
