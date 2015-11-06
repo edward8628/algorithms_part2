ae7ba5ac2f86481c18db34c3fc5bfe2518858b2d
fennnnnn <shenjian8628@gmail.com>
2015-11-05 23:15:18 -0500

    read in file string and save in data type

--- week1/WordNet.java
+++ week1/WordNet.java
@@ -2,21 +2,20 @@ import java.lang.*;
 import java.util.*;
 
 public class WordNet {
-
     Digraph graph;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms)
     {
         if (synsets == null || hypernyms == null) throw new NullPointerException();
-        if (true) throw new IllegalArgumentException();
-        //hypernyms as digraph
-        //synsets as hash table or simple table
+        //if (true) throw new IllegalArgumentException();
+        //use split to separate comma?semicolon?or space
+        //use Integer.parseInt() to parse string as int
         //readline to parse text string
 
         int size = 0;   //for V in digraph //but I can get rid of this and use table.size
 
-        //process of reading in synsets txt
+        //process of reading in synsets as simple table
         In in = new In(synsets);
         while (in.hasNextLine()) {
             size++;
@@ -25,9 +24,17 @@ public class WordNet {
 
         this.graph = new Digraph(size);
 
+        //process of reading in hypernyms as digraph
         in = new In(hypernyms);
         while (in.hasNextLine()) {
+            this.graph = new Digraph(size);
+            String[] temp = in.readLine().split(",");
+            //some lines have mnore than 2 number
+            for (int i = 1; i < temp.length; i++) {
+                graph.addEdge(Integer.parseInt(temp[0]), Integer.parseInt(temp[i])); //v->w
+            }
 
+            
         }
 
     }
