fb9f4074b59268c51cc502ee2a97e3f0a8169511
fennnnnn <shenjian8628@gmail.com>
2015-11-08 16:21:03 -0500

    make length functional with many fixable

--- week1/SAP.java
+++ week1/SAP.java
@@ -1,35 +1,61 @@
 public class SAP {
+    Digraph G;
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G)
+
+    {
+        //if () throw new java.lang.NullPointerException();
+        //if () throw new java.lang.IndexOutOfBoundsException(); //if vertex is not between 0 and G.V() - 1.
+        this.G = new Digraph(G);
+    }
+
     // length of shortest ancestral path between v and w; -1 if no such path
-    {
-
-    }
-
     public int length(int v, int w)
+
+    {
+        //if () throw new java.lang.NullPointerException();
+        //if () throw new java.lang.IndexOutOfBoundsException(); //if vertex is not between 0 and G.V() - 1.
+
+        BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(G, v);
+        //no path and return -1?
+
+        return bfs.distTo(w);
+    }
+
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    {
-        return 0;
-    }
-
     public int ancestor(int v, int w)
+
+    {
+        //if () throw new java.lang.NullPointerException();
+        //if () throw new java.lang.IndexOutOfBoundsException(); //if vertex is not between 0 and G.V() - 1.
+
+        //pathTo()?
+
+        return 0;
+    }
+
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such pat
-    {
-        return 0;
-    }
-
     public int length(Iterable<Integer> v, Iterable<Integer> w)
+
+    {
+        //if () throw new java.lang.NullPointerException();
+        //if () throw new java.lang.IndexOutOfBoundsException(); //if vertex is not between 0 and G.V() - 1.
+
+        return 0;
+    }
+
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    {
-        return 0;
-    }
-
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
-    // do unit testing of this class
+
     {
+        //if () throw new java.lang.NullPointerException();
+        //if () throw new java.lang.IndexOutOfBoundsException(); //if vertex is not between 0 and G.V() - 1.
+
         return 0;
     }
 
+    // do unit testing of this class
     public static void main(String[] args) { 
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
