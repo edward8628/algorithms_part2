2c9a773cb5b7e8cf8c20b0708e0294c741b9e54c
fennnnnn <shenjian8628@gmail.com>
2015-11-13 09:58:28 -0500

    add skeleton class seamcarver

---
+++ week2/SeamCarver.java
@@ -0,0 +1,50 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class SeamCarver {
+    // create a seam carver object based on the given pictur
+    public SeamCarver(Picture picture) {
+    }
+
+    // current picture
+    public Picture picture() {
+        return null;
+    }
+
+    // width of current picture
+    public int width() {
+        return 0;
+    }
+
+    // height of current picture
+    public int height() {
+        return 0;
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+    }
+
+    public static void main(String[] args) {
+    }
+}
