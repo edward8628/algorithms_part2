7e54d7f45e77ddb02f57b81f04080d907dc739ff
fennnnnn <shenjian8628@gmail.com>
2015-11-14 19:59:27 -0500

    finish width, height and read in ficture

--- week2/SeamCarver.java
+++ week2/SeamCarver.java
@@ -1,29 +1,45 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.Stopwatch;
+import java.awt.Color;
+import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
+    private int width;
+    private int height;
+    private double[] energy;
+    private Picture picture;
+
     // create a seam carver object based on the given pictur
     public SeamCarver(Picture picture) {
+        this.width = picture.width();
+        this.height = picture.height();
+        this.picture = picture;//new or not new?
+        //calculate every energy
+
     }
 
     // current picture
     public Picture picture() {
-        return null;
+        return this.picture;
     }
 
     // width of current picture
     public int width() {
-        return 0;
+        return width;
     }
 
     // height of current picture
     public int height() {
-        return 0;
+        return height;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        if (x < 0 || x > this.width) throw new java.lang.IndexOutOfBoundsException();
+        if (y < 0 || y > this.height) throw new java.lang.IndexOutOfBoundsException();
+
+        
         return 0;
     }
 
@@ -46,5 +62,9 @@ public class SeamCarver {
     }
 
     public static void main(String[] args) {
+        Picture pic = new Picture(args[0]);
+        SeamCarver sc = new SeamCarver(pic);
+        StdOut.println("width is " + sc.width());
+        StdOut.println("height is " + sc.height());
     }
 }
