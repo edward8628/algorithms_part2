9f4aa2f213b2d62aae4e149acf3ba97c14075e12
fennnnnn <shenjian8628@gmail.com>
2015-11-17 23:52:44 -0500

    add remove vertical and horizontal

--- week2/SeamCarver.java
+++ week2/SeamCarver.java
@@ -221,14 +221,34 @@ public class SeamCarver {
     public void removeHorizontalSeam(int[] seam) {
         if (seam == null) throw new java.lang.NullPointerException();
         if (!isValidSeam(seam, width())) throw new java.lang.IllegalArgumentException();
-
+        
+        Picture pic = new Picture(width(), height()-1);
+        for (int i = 0; i < width(); i++) {
+            for (int j = 0, k = 0; j < height(); j++) {
+                if (j != seam[i]) {
+                    pic.set(i, k, picture.get(i, j));
+                    k++;
+                }
+            }
+        }
+        this.picture = pic;
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
         if (seam == null) throw new java.lang.NullPointerException();
         if (!isValidSeam(seam, height())) throw new java.lang.IllegalArgumentException();
-
+        
+        Picture pic = new Picture(width() - 1, height());
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0, k = 0; j < width(); j++) {
+                if (j != seam[i]) {
+                    pic.set(k, i, picture.get(j, i));
+                    k++;
+                }
+            }
+        }
+        this.picture = pic;
     }
 
     public static void main(String[] args) {
