01acabbf6c68553d9adb41528d5f297e537a6a64
fennnnnn <shenjian8628@gmail.com>
2015-11-21 17:26:08 -0500

    finish the constructure and some of the methods

--- week3/BaseballElimination.java
+++ week3/BaseballElimination.java
@@ -1,46 +1,73 @@
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import java.util.HashMap;
 
 public class BaseballElimination
 {
+    private final int numberOfTeams;
+    private HashMap<String, Integer> teams;
+    private int w[]; 
+    private int l[];
+    private int r[];
+    private int g[][];
+
     // create a baseball division from given filename in format specified belo
     public BaseballElimination(String filename)
     {
+        In in = new In(filename);
+        numberOfTeams = in.readInt();
+        teams = new HashMap<String, Integer>();
+        w = new int[numberOfTeams];
+        l = new int[numberOfTeams];
+        r = new int[numberOfTeams];
+        g = new int[numberOfTeams][numberOfTeams];
+
+        for (int i = 0; i < numberOfTeams; i++) {
+            teams.put(in.readString(), i);
+            w[i] = in.readInt();
+            l[i] = in.readInt();
+            r[i] = in.readInt();
+            for (int j = 0; j < numberOfTeams; j++) {
+                if (i != j) g[i][j] = in.readInt();
+            }
+        }
     }
 
     // number of teams
     public int numberOfTeams()
     {
-        return 0;
+        return numberOfTeams;
     }
 
     // all teams
     public Iterable<String> teams()
     {
-        return null;
+        return teams.keySet();
     }
 
     // number of wins for given team
     public int wins(String team)
     {
-        return 0;
+        return w[teams.get(team)];
     }
 
     // number of losses for given team
     public int losses(String team)
     {
-        return 0;
+        return l[teams.get(team)];
     }
 
     // number of remaining games for given team
     public int remaining(String team)
     {
-        return 0;
+        return r[teams.get(team)];
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2)
     {
-        return 0;
+        return g[teams.get(team1)][teams.get(team2)];
     }
 
     // is given team eliminated?
@@ -71,9 +98,11 @@ public class BaseballElimination
 
         // % java BaseballElimination teams4.txt
         // Atlanta is not eliminated
-        // Philadelphia is eliminated by the subset R = { Atlanta New_York } New_York is not eliminated
+        // Philadelphia is eliminated by the subset R = { Atlanta New_York } 
+        // New_York is not eliminated
         // Montreal is eliminated by the subset R = { Atlanta }
-        // % java BaseballElimination teams5.txt New_York is not eliminated
+        // % java BaseballElimination teams5.txt 
+        // New_York is not eliminated
         // Baltimore is not eliminated
         // Boston is not eliminated
         // Toronto is not eliminated
