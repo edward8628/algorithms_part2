494de97e3e5c4b81c62d779eb7e72a9995e63ca4
fennnnnn <shenjian8628@gmail.com>
2015-12-10 15:43:01 -0500

    complete implementation of boggle solver

--- week5/BoggleSolver.java
+++ week5/BoggleSolver.java
@@ -2,7 +2,8 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.TrieSET;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.SET;
+//https://github.com/vinsonlee/coursera/blob/master/algs4partII-004/boggle/BoggleSolver.java
 
 public class BoggleSolver {
     private TrieSET dict;
@@ -18,19 +19,56 @@ public class BoggleSolver {
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
         if (board == null) throw new java.lang.NullPointerException();
-        Queue<String> validWords = new Queue<String>();
+        SET<String> validWords = new SET<String>();
 
         //run through board
-        //add letter to current word
-        //Q as QU and all uppercase
-        //search word if valid in dict
-        //collect valid words
+        for (int i = 0; i < board.rows(); i++) {
+            for (int j = 0; j < board.cols(); j++) {
+                boolean[][] marked = new boolean[board.rows()][board.cols()];
+                explore(board, i, j, marked, "", validWords);
+            }
+        }
 
+        //collect valid words
         return null;
     }
-    
-    private void explore() {
-    
+
+    private void explore(BoggleBoard board, int row, int col, boolean[][] marked, String prefix, SET<String> set) {
+        if (marked[row][col]) {
+            return;
+        }
+
+        //add letter to current word
+        char letter = board.getLetter(row, col);
+        String word = prefix;
+        //Q as QU and all uppercase
+        if (letter == 'Q') {
+            word += "QU";
+        } else {
+            word += letter;
+        }
+        //check if this word has prefix in our dict to save time
+        if (this.dict.longestPrefixOf(word) ==  null) {
+            return;
+        }
+        //search word if valid in dict
+        if (word.length() > 2 && dict.contains(word)) {
+            set.add(word);
+        }
+        marked[row][col] = true;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                if ((row + i >= 0) && (row + i < board.rows()) && (col + j >= 0) && (col + j < board.cols())) {
+                    explore(board, row + i, col + j, marked, word, set);
+                }
+            }
+        }
+        marked[row][col] = false;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise. 
